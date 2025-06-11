9d7b0f13dd4e194370107c267ab78e2eaf7ec6bf
hoang28092000 <hvu6097@gmail.com>
2025-06-11 14:57:41 +0700

    Fix cached files

---
+++ .history/js/news_20250611013956.js
@@ -0,0 +1,50 @@
+const isIndex = window.location.pathname === "/" || window.location.pathname.endsWith("/index.html");
+const detailPath = isIndex ? "details/detail-news.html" : "../details/detail-news.html";
+const dataPath = isIndex ? "data/news.json" : "../data/news.json";
+
+fetch(dataPath)
+  .then((response) => response.json())
+  .then((newss) => {
+    const newsList = document.querySelector(".list-news_main");
+    if (newsList) {
+      newsList.innerHTML = "";
+      newss.forEach((news) => {
+        const card = document.createElement("a");
+        card.href = `${detailPath}?id=${news.id}`;
+        card.className = "news-card";
+        card.innerHTML = `
+          <div class="news-image-container">
+            <img src="${news.image}" alt="${news.title}" class="news-image">
+          </div>
+          <div class="news-info">
+            <h3 class="news-name">${news.title}</h3>
+            <p class="news-desc">${news.desc}</p>
+          </div>
+        `;
+        newsList.appendChild(card);
+      });
+    }
+
+    // Trang chi tiết hướng dẫn
+    if (window.location.pathname.endsWith("/details/detail-news.html")) {
+      const urlParams = new URLSearchParams(window.location.search);
+      const newsId = urlParams.get("id");
+
+      if (!newsId) {
+        document.body.innerHTML = "<p>ID hướng dẫn không hợp lệ.</p>";
+        return;
+      }
+
+      const news = newss.find((item) => item.id === newsId);
+
+      if (news) {
+        document.querySelector(".news-title").textContent = news.title;
+        document.querySelector(".news-image").src = news.image;
+        document.querySelector(".news-desc").innerHTML =
+          news.desc.replace(/\n/g, "<br>");
+      } else {
+        document.body.innerHTML = "<p>tin tức không tồn tại.</p>";
+      }
+    }
+  })
+  .catch((error) => console.error("Lỗi khi tải dữ liệu:", error));
